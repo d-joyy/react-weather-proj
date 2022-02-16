2ef6af0ecd37c8f06bbd353521f539306ea0fcc4
Danielle <daniellejoyy91@gmail.com>
2022-02-16 13:38:16 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Danielle and is{" "}
-        <a href="https://github.com/d-joyy/react-weather-proj" target="_blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Danielle and is{" "}
+          <a
+            href="https://github.com/d-joyy/react-weather-proj"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
