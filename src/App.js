7c3b6a6ef27755aa3c74a26b5588a9f86d56799a
Danielle <daniellejoyy91@gmail.com>
2022-02-21 10:11:13 -0500

    Added date on the weather component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Japan" />
         <footer>
           This project was coded by Danielle and is{" "}
           <a
